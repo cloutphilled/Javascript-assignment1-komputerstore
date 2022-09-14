aac41884a23495abbb334ac2271ab3e179a7356b
Phillip Friis Petersen <phillipfriispetersen@gmail.com>
2022-09-14 15:26:00 +0200

    Added README file

--- app.js
+++ app.js
@@ -69,20 +69,22 @@ const handleAddComputer = () => {
     const cartItem = document.createElement("li");
     const lineTotal = quantity * selectedComputer.price;
 
-    cartItem.innerText = `${selectedComputer.title} $ ${selectedComputer.price} `;
+    cartItem.innerText = `${selectedComputer.title}`;
     cartElement.appendChild(cartItem);
 
     totalDue += lineTotal;
-    totalDueElement.innerText = `Total Due: $ ${totalDue.toFixed(2)}`;
+    totalDueElement.innerText = totalDue;//`Total Due: $ ${totalDue.toFixed(2)}`;
 }
 
 // This function handles the payment
 // It then checks if there's enough funds on the account
 // If not --> not enough minerals
 const handlePay = () => {
-    if (parseInt(priceElement.innerText, 10) <= parseInt(accountBalanceElement.textContent, 10)) {
-        accountBalanceElement.innerText = parseInt(accountBalanceElement.textContent,10) - parseInt(priceElement.innerText,10);
-        alert('You have succesfully purchased the ' + computerTitleElement.innerText + '!');
+    if (parseInt(totalDueElement.innerText, 10) <= parseInt(accountBalanceElement.textContent, 10)) {
+        accountBalanceElement.innerText = parseInt(accountBalanceElement.textContent,10) - parseInt(totalDueElement.innerText,10);
+        alert('You have succesfully purchased the: ' + '\n' + cartElement.innerText);
+        cartElement.innerText= " ";
+        totalDueElement.innerText = " ";
     } 
     else {
         alert('You require more minerals!');
